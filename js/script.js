a5c18fd69796cf33931be3f2482aec64e305eb4c
Lucifer <mamajanov.jasurbek23@gmail.com>
2021-06-18 22:49:03 +0500

    Second_step

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
         tabs[i].classList.add('tabheader__item_active');
     }
    
-    hideTabsContent();
+    hideTabsContent();  
     showTabsContent();
     
 
@@ -36,4 +36,98 @@ window.addEventListener('DOMContentLoaded', ()=>{
             });
         }
     });
+
+
+// timer 
+
+    const deadLine = '2021-06-28';
+    
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t/(1000 * 60 * 60  *24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24 )),
+              minutes = Math.floor((t / 1000 * 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        return{
+            'total':t,
+            'days':days,
+            'hours':hours,
+            'minutes':minutes,
+            'seconds':seconds
+        };
+    }
+
+    function getZero(num){
+        if(num >=0 && num <=10){
+            return `0${num}`;
+        }else{
+            return num;
+        }
+    }
+    
+    
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds= timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();    
+
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+            
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('.timer',deadLine);
 });
+
+
+
+
+
+
+// Тренки
+
+// let start = new Date();
+// for(let i=0;i<100000; i++){
+//     let some = i ** 3;
+// }
+
+// let end = new Date();
+
+// alert(`Цикл отработал за ${end - start} миллисекунд`);
+
+
+
+// let timeID;
+  
+    // function logger(){
+    //     if(i === 10){
+    //         clearInterval(timeID);
+    //     }
+    //     console.log('Hello');
+    //     i++;
+    // }
+    // let i = 0;
+    // let id = setTimeout(function log(){
+    //     if (i == 10){
+    //         clearInterval();
+    //     }else{
+    //         console.log("hello");
+    //         id = setTimeout(log,500);
+    //         i++; 
+    //     }
+    // },500);
